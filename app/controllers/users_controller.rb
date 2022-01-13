cddf95897e2002c1f4f77165a02c65298359b72d
Felipe Torres <felipetoress@gmail.com>
2022-01-13 18:29:24 -0300

    adicionado criacao de usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.create(user_params)
+    if @user.save
+      flash[:notice] = "Bem vindo ao Alpha_Blog #{@user.username}!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
   has_many :articles
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 5, maximum: 50}
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 50}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password
 end

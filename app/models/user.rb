ff7098f4968a1e116941bfa5790ed91336932fef
Felipe Torres <felipetoress@gmail.com>
2022-01-13 15:18:26 -0300

    adicionado validacoes de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun : 5, maximum: 50}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}
 end

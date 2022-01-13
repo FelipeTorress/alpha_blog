c87a833a444019c49ef4386f31827385a7d03a90
Felipe Torres <felipetoress@gmail.com>
2022-01-13 16:19:11 -0300

    inserindo o campo de senha no BD

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 5, maximum: 50}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
 end

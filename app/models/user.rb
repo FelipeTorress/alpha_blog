54f55557cd3f9b7e1d35eb0025be095306c30506
Felipe Torres <felipetoress@gmail.com>
2022-01-13 15:38:09 -0300

    integracao de usuarios e artigos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun : 5, maximum: 50}
-  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}
+  has_many :articles
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimun: 5, maximum: 50}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end

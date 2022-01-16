e54275254404d8dd64ae6e2d5c07e9901e53af4b
Felipe Torres <felipetoress@gmail.com>
2022-01-16 13:23:59 -0300

    view de administrador e suas permissoes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "Você so pode deletar e alterar seus artigos"
       redirect_to article_path(@article)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Sua conta foi excluida!"
     redirect_to root_path
   end
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:notice] = "Você so pode fazer alterações em sua conta"
       redirect_to user_path(@user)
     end
